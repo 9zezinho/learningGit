dfe6b3aa19c78c8554968b575f1cdb317a4c4c96
zezinho <9littlehoney@gmail.com>
2023-04-04 20:23:17 +0100

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Suresh made this change
+        System.out.println("Suresh is changing the code.");
     }
 }
